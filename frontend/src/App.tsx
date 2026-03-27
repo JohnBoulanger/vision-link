e09fa0c60a76bcd4cb858b1271b81d911b908a20
JohnBoulanger <john03yyc@gmail.com>
2026-03-27 14:42:49 -0400

    create all index.tsx and style.css files for every component/page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Route path="/" element={<Layout}>
+        <Route index element={<Public}/>
+        <Route path="*" element={<NotFound}
+      </Route>
+    </BrowserRouter>
   )
 }
 
